507162efed837f20e6412f3aeb7fdd9e2ee5bd65
Mateusz Grabowski <mxgrabowski@gmail.com>
2020-06-23 12:11:24 +0200

    change method switching css variables

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -199,7 +199,21 @@ var themeBtn = document.querySelector('.theme-switch');
 
 var setTheme = function setTheme(themeName) {
   localStorage.setItem('theme', themeName);
-  document.documentElement.className = themeName;
+  var root = document.documentElement;
+
+  if (themeName == 'theme-light') {
+    root.style.setProperty('--color-bg', '#F5F9FF');
+    root.style.setProperty('--color-blur-light', '#FFFFFF');
+    root.style.setProperty('--color-font', '#576A8A');
+    root.style.setProperty('--color-blur-dark', '#D6E0F0');
+    root.style.setProperty('--color-deep', 'transparent linear-gradient(180deg, #D6E0F0 0%, #D6E0F0 1%, #FFFFFF 100%) 0% 0% no-repeat padding-box');
+  } else {
+    root.style.setProperty('--color-bg', '#2B2F35');
+    root.style.setProperty('--color-blur-light', '#3B3E42');
+    root.style.setProperty('--color-font', '#F5F9FF');
+    root.style.setProperty('--color-blur-dark', '#181C22');
+    root.style.setProperty('--color-deep', 'transparent linear-gradient(360deg, #21262e 0%, #10131858 53%, #202020 100%) 0% 0% no-repeat padding-box');
+  }
 };
 
 var toggleTheme = function toggleTheme() {
@@ -223,6 +237,7 @@ if (localStorage.getItem('theme') === 'theme-dark') {
 }
 
 themeBtn.addEventListener('click', toggleTheme);
+themeBtn.addEventListener('touch', toggleTheme);
 "use strict";
 
 var select = document.querySelector('.search__select');
--- assets/js/custom/theme.js
+++ assets/js/custom/theme.js
@@ -2,7 +2,22 @@ const themeBtn = document.querySelector('.theme-switch');
 
 const setTheme = (themeName) => {
     localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+    let root = document.documentElement;
+
+    if (themeName == 'theme-light') {
+        root.style.setProperty('--color-bg', '#F5F9FF');
+        root.style.setProperty('--color-blur-light', '#FFFFFF');
+        root.style.setProperty('--color-font', '#576A8A');
+        root.style.setProperty('--color-blur-dark', '#D6E0F0');
+        root.style.setProperty('--color-deep', 'transparent linear-gradient(180deg, #D6E0F0 0%, #D6E0F0 1%, #FFFFFF 100%) 0% 0% no-repeat padding-box');
+    } else {
+        root.style.setProperty('--color-bg', '#2B2F35');
+        root.style.setProperty('--color-blur-light', '#3B3E42');
+        root.style.setProperty('--color-font', '#F5F9FF');
+        root.style.setProperty('--color-blur-dark', '#181C22');
+        root.style.setProperty('--color-deep', 'transparent linear-gradient(360deg, #21262e 0%, #10131858 53%, #202020 100%) 0% 0% no-repeat padding-box');
+    }
+
 }
 
 const toggleTheme = () => {
@@ -26,3 +41,4 @@ if (localStorage.getItem('theme') === 'theme-dark') {
 }
 
 themeBtn.addEventListener('click', toggleTheme);
+themeBtn.addEventListener('touch', toggleTheme);
