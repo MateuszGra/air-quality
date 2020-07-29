0384a6bb11fbe8c7899ac12e4b62e6505276876b
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2020-07-29 12:25:29 +0200

    Save raports as txt files

--- inc/raport.php
+++ inc/raport.php
@@ -1,79 +1,18 @@
 <?php
-    require_once("../src/dompdf/autoload.inc.php");
-    use Dompdf\Adapter\CPDF;
-    use Dompdf\Dompdf;
-    use Dompdf\Exception;
-
     function sendRaport($timeElapsed, $users){
         $date = date('d-m-Y H:i:s', time());
-        $body = '
-        <html>
-        <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <style>
-          body { font-family: DejaVu Sans, sans-serif; }
-          h1 { font-size: 16px; }
-          p {font-size: 12px; }
+        $body = "Data: ".$date."\r\nCzas wykonania: ".$timeElapsed."s\r\n\r\n";
 
-          table {
-            border-collapse: collapse;
-            width: 100%;
-            font-size: 12px;
-          }
-          
-          td, th {
-            border: 1px solid #000000;
-            text-align: left;
-            padding: 8px;
-          }
-          
-          tr:nth-child(even) {
-            background-color: #dddddd;
-          }
-        </style>
-        <title>RAPORT: '.$date.'</title>
-        </head>
-        <body>
 
-        <h1 style="text-align: center">Raport z mailingu</h1>
-        <p>
-            <strong>Data: '.$date.'</strong><br>
-            <strong>Czas wykonania: '.$timeElapsed.'s</strong>
-        </p>
-        ';
-
-        $body .= '
-        <table>
-        <tr>
-            <th>ID</th>
-            <th>Stacja</th>
-            <th>Powiadomienie</th>
-            <th>Indeks</th>
-        </tr>';
 
         foreach($users as $user) {
-            $body .= '
-            <tr>
-                <td>'.$user['id'].'</td>
-                <td>'.$user['station'].'</td>
-                <td>'.$user['mail'].'</td>
-                <td>'.$user['quality'].'</td>
-            </tr>
-            ';
+            $body .= "ID: ".$user['id']." | Stacja: ".$user['station']." | Powiadomienie: ".$user['mail']." | Indeks: ".$user['quality']."\r\n";
         }
 
-        $body .= '
-        </table>
-        </body>
-        </html>';
-
-        $dompdf = new Dompdf();
-        $dompdf->load_html($body);
-        $dompdf->setPaper('A4', 'portrait');
-        $dompdf->render();
-        $output = $dompdf->output();
-        $filemName = date('d_m_Y-H_i_s', time()).'.pdf';
-        file_put_contents( '../raports/'.$filemName , $output);
+        $filemName = date('d-m-Y_H-i-s', time()).'.txt';
+        $fp = fopen('../raports/'.$filemName,'w');
+        fwrite($fp, $body);
+        fclose($fp);
 
         $email = 'mxgrabowski@gmail.com';
         $subject = 'Nowy raport z aplikacji AIR';
