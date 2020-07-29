f9d7dd07a457a760b2da7725fa0929e034b1fd6f
Mateusz Grabowski <m.grabowski@rolladengroup.de>
2020-07-29 13:01:14 +0200

    save

--- inc/clean-raports.php
+++ inc/clean-raports.php
@@ -5,7 +5,7 @@
     if ($handle = opendir($path)) {
         while (false !== ($file = readdir($handle))) {
             if (is_file($path.$file))  {
-                if (filemtime($path.$file) < ( time() - ( $days * 24 * 60 * 6 ) ) )  {
+                if (filemtime($path.$file) < ( time() - ( $days * 24 * 60 * 60 ) ) )  {
                     unlink($path.$file);
                 }
             }
